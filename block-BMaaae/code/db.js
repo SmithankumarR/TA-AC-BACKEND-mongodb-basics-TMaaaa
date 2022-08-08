c0707ac19d99719ad32f725f455f7fd21e1c609b
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-08 21:18:41 +0530

    Refactor Assignment level 1

---
+++ block-BMaaae/code/db.js
@@ -0,0 +1,32 @@
+var players = [
+    
+    {
+        name:'John',
+        email : 'john@example.com',
+        age:34,
+        bid_price :" $24k"
+    },
+    {
+        name:'Marry',
+        email : 'Marry@example.com',
+        age:24,
+        bid_price :" $14k"
+    },
+    {
+        name:'Catholic',
+        email : 'catholic@example.com',
+        age:20,
+        bid_price :" $200k"
+    },
+    {
+        name:'Johmithu',
+        email : 'johnmithu@example.com',
+        age:28,
+        bid_price :" $140k"
+    }, {
+        name:'bhem',
+        email : 'bhem@example.com',
+        age:32,
+        bid_price :" $32k"
+    }
+]
