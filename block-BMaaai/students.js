646b94471c2ef6c0b9c840bc06c62cb2b4ec4f34
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-10 10:15:00 +0530

    Asiignment 3

---
+++ block-BMaaai/students.js
@@ -0,0 +1,524 @@
+var students = [
+    {
+      "_id": "62f332b0b3456ef8f0404d2a",
+      "age": 39,
+      "name": "Marsh Ryan",
+      "gender": "male",
+      "company": "GEOLOGIX",
+      "email": "marshryan@geologix.com",
+      "phone": "+1 (905) 428-3754",
+      "tags": [
+        "mollit",
+        "commodo"
+      ]
+    },
+    {
+      "_id": "62f332b085838ea1507d6ad1",
+      "age": 32,
+      "name": "Billie Howe",
+      "gender": "female",
+      "company": "ROUGHIES",
+      "email": "billiehowe@roughies.com",
+      "phone": "+1 (874) 506-2354",
+      "tags": [
+        "laborum",
+        "dolore"
+      ]
+    },
+    {
+      "_id": "62f332b0d526b288ad9068ad",
+      "age": 23,
+      "name": "Conrad Lopez",
+      "gender": "male",
+      "company": "GONKLE",
+      "email": "conradlopez@gonkle.com",
+      "phone": "+1 (954) 577-3482",
+      "tags": [
+        "dolore",
+        "culpa"
+      ]
+    },
+    {
+      "_id": "62f332b0bf2e5d91e86e92ce",
+      "age": 38,
+      "name": "Etta Pollard",
+      "gender": "female",
+      "company": "ZAJ",
+      "email": "ettapollard@zaj.com",
+      "phone": "+1 (936) 473-3171",
+      "tags": [
+        "consequat",
+        "quis"
+      ]
+    },
+    {
+      "_id": "62f332b0a0f4aecf453b2c34",
+      "age": 33,
+      "name": "Rhonda Lewis",
+      "gender": "female",
+      "company": "LOCAZONE",
+      "email": "rhondalewis@locazone.com",
+      "phone": "+1 (849) 494-3816",
+      "tags": [
+        "elit",
+        "Lorem"
+      ]
+    },
+    {
+      "_id": "62f332b0ca85bb8ebfe4ba23",
+      "age": 24,
+      "name": "Lula Harrison",
+      "gender": "female",
+      "company": "CABLAM",
+      "email": "lulaharrison@cablam.com",
+      "phone": "+1 (977) 508-2774",
+      "tags": [
+        "aliquip",
+        "voluptate"
+      ]
+    },
+    {
+      "_id": "62f332b07b2cf3c1e07ad443",
+      "age": 34,
+      "name": "Long Roy",
+      "gender": "male",
+      "company": "EPLODE",
+      "email": "longroy@eplode.com",
+      "phone": "+1 (885) 497-3596",
+      "tags": [
+        "commodo",
+        "irure"
+      ]
+    },
+    {
+      "_id": "62f332b0c9395e78ac525d4e",
+      "age": 37,
+      "name": "King Kirby",
+      "gender": "male",
+      "company": "REALYSIS",
+      "email": "kingkirby@realysis.com",
+      "phone": "+1 (938) 543-2830",
+      "tags": [
+        "ex",
+        "in"
+      ]
+    },
+    {
+      "_id": "62f332b0c048c160b4757762",
+      "age": 37,
+      "name": "Powers Moore",
+      "gender": "male",
+      "company": "SENTIA",
+      "email": "powersmoore@sentia.com",
+      "phone": "+1 (984) 577-3402",
+      "tags": [
+        "eu",
+        "nisi"
+      ]
+    },
+    {
+      "_id": "62f332b01fddd99a1ba3135c",
+      "age": 37,
+      "name": "Dudley Mason",
+      "gender": "male",
+      "company": "FANFARE",
+      "email": "dudleymason@fanfare.com",
+      "phone": "+1 (822) 423-2583",
+      "tags": [
+        "exercitation",
+        "adipisicing"
+      ]
+    },
+    {
+      "_id": "62f332b088819c320f744d1b",
+      "age": 29,
+      "name": "Rachelle Townsend",
+      "gender": "female",
+      "company": "JOVIOLD",
+      "email": "rachelletownsend@joviold.com",
+      "phone": "+1 (918) 566-3293",
+      "tags": [
+        "velit",
+        "id"
+      ]
+    },
+    {
+      "_id": "62f332b0dcd735b2575846df",
+      "age": 37,
+      "name": "Joyce Vang",
+      "gender": "female",
+      "company": "HAIRPORT",
+      "email": "joycevang@hairport.com",
+      "phone": "+1 (904) 411-3540",
+      "tags": [
+        "ut",
+        "quis"
+      ]
+    },
+    {
+      "_id": "62f332b0bcbbbd2e875857df",
+      "age": 31,
+      "name": "Geneva Acosta",
+      "gender": "female",
+      "company": "DAISU",
+      "email": "genevaacosta@daisu.com",
+      "phone": "+1 (981) 583-3589",
+      "tags": [
+        "proident",
+        "esse"
+      ]
+    },
+    {
+      "_id": "62f332b068940209524aa43b",
+      "age": 28,
+      "name": "Estella Armstrong",
+      "gender": "female",
+      "company": "EXOBLUE",
+      "email": "estellaarmstrong@exoblue.com",
+      "phone": "+1 (972) 522-2889",
+      "tags": [
+        "laboris",
+        "culpa"
+      ]
+    },
+    {
+      "_id": "62f332b0f0744cc3fafffb70",
+      "age": 36,
+      "name": "Delgado Haley",
+      "gender": "male",
+      "company": "ASSURITY",
+      "email": "delgadohaley@assurity.com",
+      "phone": "+1 (808) 581-3814",
+      "tags": [
+        "elit",
+        "sunt"
+      ]
+    },
+    {
+      "_id": "62f332b0220f1705b33b984e",
+      "age": 32,
+      "name": "Ora Hernandez",
+      "gender": "female",
+      "company": "ANIXANG",
+      "email": "orahernandez@anixang.com",
+      "phone": "+1 (973) 422-3173",
+      "tags": [
+        "pariatur",
+        "id"
+      ]
+    },
+    {
+      "_id": "62f332b0ca28e1b9445f3f4d",
+      "age": 38,
+      "name": "Mabel Lane",
+      "gender": "female",
+      "company": "GLASSTEP",
+      "email": "mabellane@glasstep.com",
+      "phone": "+1 (905) 469-2102",
+      "tags": [
+        "nulla",
+        "anim"
+      ]
+    },
+    {
+      "_id": "62f332b0b61c1936ad0859d7",
+      "age": 39,
+      "name": "Goodman Bowman",
+      "gender": "male",
+      "company": "KIOSK",
+      "email": "goodmanbowman@kiosk.com",
+      "phone": "+1 (841) 580-3950",
+      "tags": [
+        "anim",
+        "Lorem"
+      ]
+    },
+    {
+      "_id": "62f332b010b080437f7a3a06",
+      "age": 34,
+      "name": "Sherman Park",
+      "gender": "male",
+      "company": "FRENEX",
+      "email": "shermanpark@frenex.com",
+      "phone": "+1 (829) 592-2272",
+      "tags": [
+        "dolore",
+        "ullamco"
+      ]
+    },
+    {
+      "_id": "62f332b0b8124110404cadb9",
+      "age": 24,
+      "name": "Roxanne Hays",
+      "gender": "female",
+      "company": "GAPTEC",
+      "email": "roxannehays@gaptec.com",
+      "phone": "+1 (825) 584-2567",
+      "tags": [
+        "duis",
+        "sit"
+      ]
+    },
+    {
+      "_id": "62f332b0f00ad45025e8c222",
+      "age": 40,
+      "name": "Latonya Nixon",
+      "gender": "female",
+      "company": "ZEDALIS",
+      "email": "latonyanixon@zedalis.com",
+      "phone": "+1 (956) 422-3365",
+      "tags": [
+        "occaecat",
+        "ad"
+      ]
+    },
+    {
+      "_id": "62f332b0c3a003cfed8a643a",
+      "age": 23,
+      "name": "Daugherty Carlson",
+      "gender": "male",
+      "company": "OPTICON",
+      "email": "daughertycarlson@opticon.com",
+      "phone": "+1 (802) 472-2639",
+      "tags": [
+        "Lorem",
+        "occaecat"
+      ]
+    },
+    {
+      "_id": "62f332b06303aadaab4bd254",
+      "age": 31,
+      "name": "Sparks Flowers",
+      "gender": "male",
+      "company": "EURON",
+      "email": "sparksflowers@euron.com",
+      "phone": "+1 (899) 591-3772",
+      "tags": [
+        "aliquip",
+        "pariatur"
+      ]
+    },
+    {
+      "_id": "62f332b02018c86d9c5977a9",
+      "age": 29,
+      "name": "Mollie Stout",
+      "gender": "female",
+      "company": "MULTRON",
+      "email": "molliestout@multron.com",
+      "phone": "+1 (963) 549-3462",
+      "tags": [
+        "adipisicing",
+        "exercitation"
+      ]
+    },
+    {
+      "_id": "62f332b0520a5c4769e017f2",
+      "age": 38,
+      "name": "Mccormick Howell",
+      "gender": "male",
+      "company": "GEEKFARM",
+      "email": "mccormickhowell@geekfarm.com",
+      "phone": "+1 (888) 449-2405",
+      "tags": [
+        "commodo",
+        "incididunt"
+      ]
+    },
+    {
+      "_id": "62f332b0d98b92208d3f17b9",
+      "age": 37,
+      "name": "Forbes Lang",
+      "gender": "male",
+      "company": "FLEETMIX",
+      "email": "forbeslang@fleetmix.com",
+      "phone": "+1 (942) 595-3880",
+      "tags": [
+        "quis",
+        "irure"
+      ]
+    },
+    {
+      "_id": "62f332b0a16f4f3e75be86d4",
+      "age": 23,
+      "name": "Dorothea Ramsey",
+      "gender": "female",
+      "company": "REPETWIRE",
+      "email": "dorothearamsey@repetwire.com",
+      "phone": "+1 (856) 464-3607",
+      "tags": [
+        "aliquip",
+        "proident"
+      ]
+    },
+    {
+      "_id": "62f332b0227b9c012bd507be",
+      "age": 34,
+      "name": "Snider Fox",
+      "gender": "male",
+      "company": "SULFAX",
+      "email": "sniderfox@sulfax.com",
+      "phone": "+1 (855) 496-3124",
+      "tags": [
+        "ipsum",
+        "aute"
+      ]
+    },
+    {
+      "_id": "62f332b01414967f417e8043",
+      "age": 21,
+      "name": "Clemons Forbes",
+      "gender": "male",
+      "company": "OPTYK",
+      "email": "clemonsforbes@optyk.com",
+      "phone": "+1 (862) 497-2857",
+      "tags": [
+        "occaecat",
+        "laborum"
+      ]
+    },
+    {
+      "_id": "62f332b0ac27a758a87813a4",
+      "age": 26,
+      "name": "Decker Dunn",
+      "gender": "male",
+      "company": "EMERGENT",
+      "email": "deckerdunn@emergent.com",
+      "phone": "+1 (897) 563-3685",
+      "tags": [
+        "do",
+        "proident"
+      ]
+    },
+    {
+      "_id": "62f332b0f46a9cc1d88dc178",
+      "age": 32,
+      "name": "Berta Welch",
+      "gender": "female",
+      "company": "BITENDREX",
+      "email": "bertawelch@bitendrex.com",
+      "phone": "+1 (930) 478-3847",
+      "tags": [
+        "voluptate",
+        "Lorem"
+      ]
+    },
+    {
+      "_id": "62f332b0b3b73b6107291292",
+      "age": 39,
+      "name": "Helena Manning",
+      "gender": "female",
+      "company": "LUDAK",
+      "email": "helenamanning@ludak.com",
+      "phone": "+1 (862) 461-2616",
+      "tags": [
+        "sint",
+        "amet"
+      ]
+    },
+    {
+      "_id": "62f332b02d165a2d0013bb95",
+      "age": 36,
+      "name": "Caitlin Farrell",
+      "gender": "female",
+      "company": "MITROC",
+      "email": "caitlinfarrell@mitroc.com",
+      "phone": "+1 (865) 572-3107",
+      "tags": [
+        "occaecat",
+        "sit"
+      ]
+    },
+    {
+      "_id": "62f332b0c95627c3d3d86386",
+      "age": 20,
+      "name": "Evans Bray",
+      "gender": "male",
+      "company": "GALLAXIA",
+      "email": "evansbray@gallaxia.com",
+      "phone": "+1 (978) 563-2235",
+      "tags": [
+        "elit",
+        "do"
+      ]
+    },
+    {
+      "_id": "62f332b0f6ba43fa461ac76f",
+      "age": 33,
+      "name": "Frankie Mccormick",
+      "gender": "female",
+      "company": "OMATOM",
+      "email": "frankiemccormick@omatom.com",
+      "phone": "+1 (992) 507-2740",
+      "tags": [
+        "id",
+        "consequat"
+      ]
+    },
+    {
+      "_id": "62f332b0ada58826f9766951",
+      "age": 23,
+      "name": "Phyllis Chavez",
+      "gender": "female",
+      "company": "EARTHPLEX",
+      "email": "phyllischavez@earthplex.com",
+      "phone": "+1 (965) 556-3201",
+      "tags": [
+        "laboris",
+        "qui"
+      ]
+    },
+    {
+      "_id": "62f332b095e57cbcc54928dd",
+      "age": 32,
+      "name": "Myrtle Luna",
+      "gender": "female",
+      "company": "NIKUDA",
+      "email": "myrtleluna@nikuda.com",
+      "phone": "+1 (915) 488-3973",
+      "tags": [
+        "incididunt",
+        "exercitation"
+      ]
+    },
+    {
+      "_id": "62f332b0da551cb8654a4487",
+      "age": 37,
+      "name": "Diane Henry",
+      "gender": "female",
+      "company": "GEEKWAGON",
+      "email": "dianehenry@geekwagon.com",
+      "phone": "+1 (916) 505-3226",
+      "tags": [
+        "cillum",
+        "qui"
+      ]
+    },
+    {
+      "_id": "62f332b0cdad66ad7932562d",
+      "age": 24,
+      "name": "Collins Holt",
+      "gender": "male",
+      "company": "ZIDOX",
+      "email": "collinsholt@zidox.com",
+      "phone": "+1 (824) 555-2863",
+      "tags": [
+        "aliquip",
+        "reprehenderit"
+      ]
+    },
+    {
+      "_id": "62f332b0a82816c81518ec49",
+      "age": 38,
+      "name": "Pearl Ward",
+      "gender": "female",
+      "company": "VETRON",
+      "email": "pearlward@vetron.com",
+      "phone": "+1 (828) 577-2784",
+      "tags": [
+        "irure",
+        "ipsum"
+      ]
+    }
+  ]
+
+  db.students.insertMany(students)
